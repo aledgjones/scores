66c4c1bf330be4ff38423db7bcfd9653d1c86ddf
Aled Jones <aledjones.viola@gmail.com>
2022-06-18 10:22:17 +0100

    reduct auth wait

--- src/states/AuthCheck.tsx
+++ src/states/AuthCheck.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUid } from "../services/auth";
-import { supabase } from "../services/db";
 import Spinner from "../ui/components/spinner";
 
 export const AuthCheck = () => {
@@ -14,7 +13,7 @@ export const AuthCheck = () => {
     } else {
       const ref = setTimeout(() => {
         navigate("/login");
-      }, 2000);
+      }, 1000);
       return () => {
         clearTimeout(ref);
       };

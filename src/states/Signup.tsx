a04c888b2776c3d1bedbce7cb85fa86b9405d739
Aled Jones <aledjones.viola@gmail.com>
2023-02-23 23:00:49 +0000

    fix: stop signup

--- src/states/Signup.tsx
+++ src/states/Signup.tsx
@@ -1,38 +1,10 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import pkg from "../../package.json";
-import toast from "react-hot-toast";
-import { signUp } from "../services/auth";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Card from "../ui/components/card";
 import CardContent from "../ui/components/card-content";
-import Input from "../ui/components/input";
-import Checkbox from "../ui/components/checkbox";
-import Button from "../ui/components/button";
 
 export const Signup: FC = () => {
-  const navigate = useNavigate();
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [terms, setTerms] = useState(false);
-  const [working, setWorking] = useState(false);
-
-  const submit = async () => {
-    if (working) {
-      return;
-    }
-
-    try {
-      setWorking(true);
-      await signUp(name, email, password, terms);
-      navigate("/library");
-    } catch (error) {
-      toast.error(error.message);
-      setWorking(false);
-    }
-  };
-
   return (
     <>
       <div className="signup">
@@ -40,54 +12,15 @@ export const Signup: FC = () => {
           <CardContent>
             <h1 className="logo">{pkg.displayName}</h1>
             <p className="version">Version {pkg.version}</p>
-            <form onSubmit={(e) => e.preventDefault()}>
-              <Input
-                label="Name"
-                type="text"
-                value={name}
-                onChange={(val) => setName(val)}
-                margin
-              />
-              <Input
-                label="Email"
-                type="email"
-                value={email}
-                onChange={(val) => setEmail(val)}
-                margin
-              />
-              <Input
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(val) => setPassword(val)}
-                margin
-              />
-
-              <div className="checkbox" onClick={() => setTerms((s) => !s)}>
-                <Checkbox value={terms} />
-                <p>
-                  <span>I agree to the </span>
-                  <Link
-                    className="link"
-                    to="/terms-and-conditions"
-                    onClick={(e) => e.stopPropagation()}
-                    target="_blank"
-                  >
-                    terms and conditions
-                  </Link>
-                  <span> of service</span>
-                </p>
-              </div>
-
-              <div className="buttons">
-                <Link to="/login" className="link">
-                  Back
-                </Link>
-                <Button primary onClick={submit} className="login-button">
-                  Signup
-                </Button>
-              </div>
-            </form>
+            <p className="info">
+              Thanks for your interest in {pkg.displayName}, we are currently in
+              testing and hope to be available soon.
+            </p>
+            <div className="buttons">
+              <Link to="/login" className="link">
+                Back
+              </Link>
+            </div>
           </CardContent>
         </Card>
       </div>
@@ -136,6 +69,9 @@ export const Signup: FC = () => {
           align-items: center;
           margin-bottom: 20px;
           cursor: pointer;
+        }
+        .info {
+          margin-bottom: 20px;
         }
         .buttons {
           display: flex;
